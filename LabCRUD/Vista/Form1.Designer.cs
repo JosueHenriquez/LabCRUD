9972b656b20c952ae76cbcfbec97ba6d4a545bc5
JosueHenriquez <52579357+JosueHenriquez@users.noreply.github.com>
2021-07-16 14:20:52 -0600

    Boton de mensaje de prueba

---
+++ LabCRUD/Vista/Form1.Designer.cs
@@ -0,0 +1,61 @@
+﻿namespace Vista
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.BtnPrueba = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // BtnPrueba
+            // 
+            this.BtnPrueba.Location = new System.Drawing.Point(175, 69);
+            this.BtnPrueba.Name = "BtnPrueba";
+            this.BtnPrueba.Size = new System.Drawing.Size(185, 94);
+            this.BtnPrueba.TabIndex = 0;
+            this.BtnPrueba.Text = "Mensaje";
+            this.BtnPrueba.UseVisualStyleBackColor = true;
+            this.BtnPrueba.Click += new System.EventHandler(this.BtnPrueba_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(505, 249);
+            this.Controls.Add(this.BtnPrueba);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button BtnPrueba;
+    }
+}
+
